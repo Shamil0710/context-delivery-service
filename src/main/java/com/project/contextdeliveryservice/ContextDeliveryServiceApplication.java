b57df5d4ca402433abdc4f0d29b6a3d34e05c321
Shamil <Shamil0710@mail.ru>
2021-06-08 15:36:05 +0300

    Init

---
+++ src/main/java/com/project/contextdeliveryservice/ContextDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.contextdeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContextDeliveryServiceApplication.class, args);
+    }
+
+}
